00683f8e9b08ef2f51e485b61fbb96a23d89c254
Valeria Silveira <ssilveira@nosis.com>
2023-04-03 09:04:04 -0300

    add root & css styles

---
+++ app/root.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Meta, Links } from "@remix-run/react";
+import styles from "./styles/index.css";
+
+export const meta = () => {
+  return [
+    { charset: "utf-8" },
+    { title: "GuitarLA - Remix" },
+    { viewport: "width=device-width,initial-scale=1" },
+  ];
+};
+
+export const links = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: "https://necolas.github.io/normalize.css/8.0.1/normalize.css",
+    },
+    {
+      rel: "preconnect",
+      href: "https://fonts.googleapis.com",
+    },
+    {
+      rel: "preconnect",
+      href: "https://fonts.gstatic.com",
+      crossOrigin: "true",
+    },
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Outfit:wght@400;700;900&display=swap",
+    },
+    {
+      rel: "stylesheet",
+      href: styles,
+    },
+  ];
+};
+
+const App = () => {
+  return (
+    <Document>
+      <h1>Title</h1>
+    </Document>
+  );
+};
+
+const Document = ({ children }) => {
+  return (
+    <html lang="es">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+};
+
+export default App;
