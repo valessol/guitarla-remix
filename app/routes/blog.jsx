9c8749d4beb7c25316ad83449f83a6bf2872651c
Valeria Silveira <ssilveira@nosis.com>
2023-04-05 14:59:23 -0300

    edit blog page & post component

--- app/routes/blog.jsx
+++ app/routes/blog.jsx
@@ -1,7 +1,34 @@
+import { useLoaderData } from "@remix-run/react";
 import React from "react";
+import { getAllPosts } from "~/api/blog.server";
+import Post from "~/components/post";
+import styles from "~/styles/blog.css";
+
+export const links = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: styles,
+    },
+  ];
+};
+
+export const loader = async () => {
+  return await getAllPosts();
+};
 
 const Blog = () => {
-  return <div>blog</div>;
+  const posts = useLoaderData();
+  return (
+    <main className="contenedor">
+      <h2 className="heading">Blog</h2>
+      <div className="blog">
+        {posts.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
+      </div>
+    </main>
+  );
 };
 
 export default Blog;
